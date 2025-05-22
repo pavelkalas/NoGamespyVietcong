5f02e4cddba4e1d2f97aec720b2b6bfb88c67551
Pavel Kalaš (Floxen) <pavelkalas2048@seznam.cz>
2025-05-22 17:11:01 +0200

    Pridany tridy pro praci s masterserverama, a model masterserveru Master.cs

---
+++ Master.cs
@@ -0,0 +1,14 @@
+﻿namespace NoGamespyVietcong
+{
+    class Master
+    {
+        public string IpAddress;
+        public long Ping;
+
+        public Master(string ipAddress, long ping = -1)
+        {
+            this.IpAddress = ipAddress;
+            this.Ping = ping;
+        }
+    }
+}
---
+++ Masterserver.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+/**
+ * NoGamespyVietcong v1.0
+ * by Pavel Kalaš 2025 (Floxen).
+ * -----------------------------
+ * https://github.com/pavelkalas/NoGamespyVietcong
+ */
+
+namespace NoGamespyVietcong
+{
+    class Masterserver
+    {
+        /// <summary>
+        /// Instance pingování
+        /// </summary>
+        private static readonly Ping ping = new Ping();
+
+        /// <summary>
+        /// List funkčních masterserverů
+        /// </summary>
+        private static readonly List<Master> masterList = new List<Master>()
+        {
+            new Master("46.28.109.117"),         // vietcong1.eu  MASTER
+            new Master("85.255.3.25"),           // alpha-team.cz MASTER
+        };
+
+        /// <summary>
+        /// Získá nejlepší masterserver podle odezvy
+        /// </summary>
+        /// <returns>Vrací IP masterserveru s nejlepší odezvou</returns>
+        public static string GetBestConnectionMaster()
+        {
+            foreach (var master in masterList)
+            {
+                lock (masterList)
+                {
+                    long pingResult = PingMasterServer(master.IpAddress);
+
+                    lock (master)
+                    {
+                        master.Ping = pingResult;
+                    }
+                }
+            }
+
+            // vymaže nedostupné masterservery
+            masterList.RemoveAll(mst => mst.Ping == 0);
+
+            // navrátí nejlepší master podle odezvy
+            return masterList.OrderBy(mst => mst.Ping).ToList().FirstOrDefault().IpAddress;
+        }
+
+        /// <summary>
+        /// Pingne masterserver a navrátí odezvu
+        /// </summary>
+        /// <param name="ipAddress">IP adresa masterserveru</param>
+        /// <param name="timeOut">Maximální čas snažit se o pingnutí</param>
+        /// <returns>Odezva masterserveru</returns>
+        private static long PingMasterServer(string ipAddress, int timeOut = 2000)
+        {
+            return ping.Send(ipAddress, timeOut).RoundtripTime;
+        }
+    }
+}
